273052742433b81fbdbab002b57f9c953b899efa
Young143 <young143@yeah.net>
2026-05-07 16:47:23 +0800

    refactor: add clang-format config and reorganize files into SFDL directory

--- template.cpp
+++ template.cpp
@@ -1,17 +1,18 @@
-//比赛默认模板
 #include <bits/stdc++.h>
 #define endl "\n"
-#define rep(i, a, b) for (i64 i = a; i <= b; i++)
-#define lep(i, a, b) for (i64 i = a; i >= b; i--)
 // #define MOD
 using namespace std;
 using i64 = int64_t;
 using ui64 = uint64_t;
 using vi64 = vector<i64>;
+using ll = i64;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
-
+    ll t;
+    cin >> t;
+    while (t--) {
+    }
     return 0;
 }
